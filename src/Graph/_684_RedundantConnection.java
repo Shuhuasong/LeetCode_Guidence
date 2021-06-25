d13364553e45754dde8a345d0965e24268871f26
Shuhuasong <shuhuasong888@gmail.com>
2021-06-25 16:20:48 -0400

    LeetCode

---
+++ src/Graph/_684_RedundantConnection.java
@@ -0,0 +1,89 @@
+package Graph;
+
+public class _684_RedundantConnection {
+    //Union Find
+    //Time O(n) Space = O(n)
+    class UnionFind{
+        int[] parents;
+        int[] rank;
+        public UnionFind(int n){
+            parents = new int[n];
+            rank = new int[n];
+            for(int i=0; i<n; i++){
+                parents[i] = i;
+                rank[i] = 1;
+            }
+        }
+
+        public boolean union(int x, int y){
+            int px = find(x);
+            int py = find(y);
+            if(px==py) return false;
+            if(rank[px] < rank[py]){
+                parents[px] = py;
+            }else if(rank[py] < rank[px]){
+                parents[py] = px;
+            }else{
+                parents[py] = px;
+                rank[px] += 1;
+            }
+            return true;
+        }
+        //find the parent of x and path compression
+        public int find(int x){
+            while(parents[x] != x){
+                parents[x] = parents[parents[x]];
+                x = parents[x];
+            }
+            return x;
+        }
+    }
+
+    public int[] findRedundantConnection(int[][] edges) {
+        UnionFind  u = new UnionFind(edges.length+1);
+        for(int[] e : edges){
+            if(!u.union(e[0], e[1])){
+                return e;
+            }
+        }
+        return null;
+    }
+
+
+    /*
+       //DFS traversal
+    //Time = O(n^2) Space = O(n)
+   public int[] findRedundantConnection(int[][] edges) {
+        Map<Integer, List<Integer>> graph = new HashMap<>();
+
+        for(int[] e : edges){
+            int u = e[0];
+            int v = e[1];
+            Set<Integer> visited = new HashSet<>();
+            if(hasPath(u, v, graph, visited)){
+                return e;
+            }
+            if(!graph.containsKey(u)){
+                graph.put(u, new ArrayList<>());
+            }
+            graph.get(u).add(v);
+            if(!graph.containsKey(v)){
+                graph.put(v, new ArrayList<>());
+            }
+            graph.get(v).add(u);
+        }
+        return new int[]{};
+    }
+
+    private boolean hasPath(int src, int des, Map<Integer, List<Integer>> graph, Set<Integer> visited){
+        if(!graph.containsKey(src) || !graph.containsKey(des)) return false;
+        if(src==des) return true;
+        visited.add(src);
+        for(int nei : graph.get(src)){
+            if(visited.contains(nei)) continue;
+            if(hasPath(nei, des, graph, visited)) return true;
+        }
+        return false;
+    }
+    */
+}
